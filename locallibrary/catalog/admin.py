3dfb489602700c2bdb5401f8dd5bebb6c0b2eca5
JohnsonMuriuki <mwangimjohnson@gmail.com>
2023-12-04 16:20:28 +0300

    Changes in models and admin site
    Changes done on models and admin files

--- locallibrary/catalog/admin.py
+++ locallibrary/catalog/admin.py
@@ -1,3 +1,45 @@
 from django.contrib import admin
 
+from .models import Book, Author, Genre, BookInstance, Language
+
+
 # Register your models here.
+
+
+class AuthorAdmin(admin.ModelAdmin):
+    list_display = ('last_name', 'first_name', 'date_of_birth', 'date_of_death')
+
+
+admin.site.register(Author, AuthorAdmin)
+
+
+class BookInstanceInline(admin.TabularInline):
+    model = BookInstance
+
+
+class BookAdmin(admin.ModelAdmin):
+    list_display = ('isbn', 'title', 'author', 'display_genre')
+
+    inlines = [BookInstanceInline]
+
+
+admin.site.register(Book, BookAdmin)
+
+
+class BookInstanceAdmin(admin.ModelAdmin):
+    list_filter = ('status', 'due_back')
+    fieldsets = (
+        (None, {
+            'fields': ('book', 'imprint', 'id')
+        }),
+        ('Availability', {
+            'fields': ('status', 'due_back')
+        }),
+    )
+
+
+admin.site.register(BookInstance, BookInstanceAdmin)
+
+admin.site.register(Language)
+
+admin.site.register(Genre)
