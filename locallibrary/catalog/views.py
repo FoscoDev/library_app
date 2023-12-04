c32f6f1989715b10ccac3388e9dc13db5d1bc679
JohnsonMuriuki <mwangimjohnson@gmail.com>
2023-12-04 17:13:44 +0300

    Adding Templates
    Displaying content dynamically in templates

--- locallibrary/catalog/views.py
+++ locallibrary/catalog/views.py
@@ -1,3 +1,26 @@
 from django.shortcuts import render
 
+from .models import BookInstance, Book, Author
+
+
 # Create your views here.
+
+
+def index(request):
+    num_books = Book.objects.all().count()
+    num_instances = BookInstance.objects.all().count()
+
+    num_instances_available = BookInstance.objects.filter(status='a').count()
+
+    num_authors = Author.objects.count()
+
+    context = {
+        'num_books': num_books,
+        'num_instances': num_instances,
+        'num_instances_available': num_instances_available,
+        'num_authors': num_authors,
+    }
+
+    return render(request, 'catalog/index.html', context=context)
+
+
